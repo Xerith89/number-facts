645194703fdf1893c8412a5436aca5bb2942b55d
Alex Coupe <acoupe@gmail.com>
2019-04-21 14:33:02 +0100

    Ajax function implemented. Added PHP page to allow Heroku build

---
+++ fact.js
@@ -0,0 +1,39 @@
+//add event listener to our input field
+
+document.querySelector('#number').addEventListener('input', (e) => getFact());
+
+function getFact()
+{
+    let number = document.querySelector('#number').value;
+    let url = `http://numbersapi.com/${number}/trivia`;
+    let factTitle = document.querySelector('#num-fact-title');
+    let factBody = document.querySelector('#num-fact');
+    const title = '<h3>Number Fact</h3>';
+    if (number == '')
+    {
+        factTitle.style.display = "none";
+        factBody.style.display = "none";
+    } else {
+        fetch(url)
+        .then((res) => {
+        if (res.ok) {
+        return res.text()
+        } else {
+            return Promise.reject("Error: "+res.statusText())
+        }
+    })
+        .then((data) => {
+            //Take our data and insert into dom
+            factTitle.style.display = "initial";
+            factBody.style.display = "initial";
+            factTitle.innerHTML = title;
+            factBody.innerHTML = `<p class="mb-4 card-body card-text">${data}</p>`;
+        })
+        .catch((error) =>  {
+            factTitle.style.display = "initial";
+            factBody.style.display = "initial";
+            factTitle.innerHTML = '<h3 class="text-danger">Error, fact not found!</h3>';
+            factBody.innerHTML = '';
+        });
+    }
+}
